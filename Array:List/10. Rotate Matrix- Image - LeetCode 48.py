dfcf4d9264945939d2162438ad54b9a4754c8c91
Sanjana Reddy Gurrappagaru <gsanjanareddy459@gmail.com>
2025-06-17 18:49:20 +0530

    Day 2 updated

--- Array:List/10. Rotate Matrix- Image - LeetCode 48.py
+++ Array:List/10. Rotate Matrix- Image - LeetCode 48.py
@@ -8,3 +8,18 @@
 
 # Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 # Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+def rotate(matrix):
+    n = len(matrix)
+
+    # Step 1: Transpose
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse each row
+    for row in matrix:
+        row.reverse()
+
+TC: O(n^2)
+SC: O(1)
